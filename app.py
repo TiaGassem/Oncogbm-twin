461c97d0a8402611fd9d482a5d5541a9759a0e60
TiaGassem <188719560+TiaGassem@users.noreply.github.com>
2026-08-12 22:15:35 +0100

    Fix import formatting for scipy.optimize

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import requests
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit 
 import streamlit as st
 import streamlit.components.v1 as components
 
